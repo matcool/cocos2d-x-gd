7cb196aee2d7a03a6ad3bff2296f869debf4e9d5
matcool <26722564+matcool@users.noreply.github.com>
2024-11-23 12:04:51 -0300

    keyboard and mouse input (partial)

--- cocos2dx/platform/win32/CCEGLView.cpp
+++ cocos2dx/platform/win32/CCEGLView.cpp
@@ -32,6 +32,8 @@ THE SOFTWARE.
 #include "keypad_dispatcher/CCKeypadDispatcher.h"
 #include "support/CCPointExtension.h"
 #include "CCApplication.h"
+#include "robtop/keyboard_dispatcher/CCKeyboardDispatcher.h"
+#include "robtop/mouse_dispatcher/CCMouseDispatcher.h"
 
 #define GLFW_EXPOSE_NATIVE_WIN32
 #define GLFW_EXPOSE_NATIVE_WGL
@@ -235,9 +237,103 @@ bool CCEGLView::initWithRect(const std::string& name, cocos2d::CCRect rect, floa
 
     glfwMakeContextCurrent(m_pMainWindow);
 
+    glfwSetKeyCallback(m_pMainWindow, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        CCEGLView::sharedOpenGLView()->onGLFWKeyCallback(window, key, scancode, action, mods);
+    });
+    glfwSetMouseButtonCallback(m_pMainWindow, [](GLFWwindow* window, int button, int action, int mods) {
+        CCEGLView::sharedOpenGLView()->onGLFWMouseCallBack(window, button, action, mods);
+    });
+
     this->initGlew();
 
     return true;
+}
+
+void CCEGLView::onGLFWKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    bool isKeyDown = action != GLFW_RELEASE;
+    bool isKeyRepeat = action == GLFW_REPEAT;
+
+    if (key == GLFW_KEY_BACKSPACE && isKeyDown) {
+        CCIMEDispatcher::sharedDispatcher()->dispatchDeleteBackward();
+    }
+
+    auto* dir = CCDirector::sharedDirector();
+    auto* kb = dir->getKeyboardDispatcher();
+
+    static std::unordered_map<int, enumKeyCodes> keyMap = {
+        {GLFW_KEY_SPACE, KEY_Space},
+        {GLFW_KEY_ESCAPE, KEY_Escape},
+        {GLFW_KEY_ENTER, KEY_Enter},
+        {GLFW_KEY_TAB, KEY_Tab},
+        {GLFW_KEY_BACKSPACE, KEY_Backspace},
+        {GLFW_KEY_INSERT, KEY_Insert},
+        {GLFW_KEY_DELETE, KEY_Delete},
+        {GLFW_KEY_RIGHT, KEY_ArrowRight},
+        {GLFW_KEY_LEFT, KEY_ArrowLeft},
+        {GLFW_KEY_DOWN, KEY_ArrowDown},
+        {GLFW_KEY_UP, KEY_ArrowUp},
+        {GLFW_KEY_PAGE_UP, KEY_PageUp},
+        {GLFW_KEY_PAGE_DOWN, KEY_PageDown},
+        {GLFW_KEY_HOME, KEY_Home},
+        {GLFW_KEY_END, KEY_End},
+        {GLFW_KEY_CAPS_LOCK, KEY_CapsLock},
+        {GLFW_KEY_SCROLL_LOCK, KEY_ScrollLock},
+        {GLFW_KEY_NUM_LOCK, KEY_Numlock},
+        {GLFW_KEY_PRINT_SCREEN, KEY_PrintScreen},
+        {GLFW_KEY_PAUSE, KEY_Pause},
+        {GLFW_KEY_F1, KEY_F1},
+        {GLFW_KEY_F2, KEY_F2},
+        {GLFW_KEY_F3, KEY_F3},
+        {GLFW_KEY_F4, KEY_F4},
+        {GLFW_KEY_F5, KEY_F5},
+        {GLFW_KEY_F6, KEY_F6},
+        {GLFW_KEY_F7, KEY_F7},
+        {GLFW_KEY_F8, KEY_F8},
+        {GLFW_KEY_F9, KEY_F9},
+        {GLFW_KEY_F10, KEY_F10},
+        {GLFW_KEY_F11, KEY_F11},
+        {GLFW_KEY_F12, KEY_F12},
+        {GLFW_KEY_F13, KEY_F13},
+        {GLFW_KEY_F14, KEY_F14},
+        {GLFW_KEY_F15, KEY_F15},
+        {GLFW_KEY_F16, KEY_F16},
+        {GLFW_KEY_F17, KEY_F17},
+        {GLFW_KEY_F18, KEY_F18},
+        {GLFW_KEY_F19, KEY_F19},
+        {GLFW_KEY_F20, KEY_F20},
+        {GLFW_KEY_F21, KEY_F21},
+        {GLFW_KEY_F22, KEY_F22},
+    };
+    
+    enumKeyCodes keyCode = KEY_None;
+    if (auto it = keyMap.find(key); it != keyMap.end()) {
+        keyCode = it->second;
+    }
+    keyCode = kb->convertKeyCode(keyCode);
+    CCLOG("key %d %d %d %d", key, scancode, action, mods);
+
+    // TODO: check ime delegate
+
+    CCDirector::sharedDirector()->getKeyboardDispatcher()->dispatchKeyboardMSG(keyCode, isKeyDown, isKeyRepeat);
+}
+
+void CCEGLView::onGLFWMouseCallBack(GLFWwindow* window, int button, int action, int mods) {
+    auto* dir = CCDirector::sharedDirector();
+    auto* mouse = dir->getMouseDispatcher();
+
+    // TODO: some stuff with set window pos??
+
+    int touchID = 0;
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+        if (action == GLFW_PRESS) {
+            m_bCaptured = true;
+            // TODO: mouse locked stuff
+            this->handleTouchesBegin(1, &touchID, &m_fMouseX, &m_fMouseY);
+        } else if (action == GLFW_RELEASE) {
+            m_bCaptured = false;
+            this->handleTouchesEnd(1, &touchID, &m_fMouseX, &m_fMouseY);
+        }
+    }
 }
 
 bool CCEGLView::initGlew() {
@@ -263,6 +359,11 @@ bool CCEGLView::windowShouldClose() {
 }
 
 void CCEGLView::pollEvents() {
+    double x, y;
+    glfwGetCursorPos(m_pMainWindow, &x, &y);
+    m_fMouseX = x;
+    m_fMouseY = y;
+
     glfwPollEvents();
 }
 
